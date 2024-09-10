7d45fe29ff703f5e7cd650a73aba032ca4bd525b
FloppyDuck <158211952+Azrafdabomb@users.noreply.github.com>
2024-09-10 16:54:29 -0400

    Lab 1 - Azraf Hossain
    gfskjhlgrsligfs;fygrzs;iusgrhligufagfrliuhgre;oiugry;oy;gy

---
+++ Azraf_Hossain_Lab_1.cpp
@@ -0,0 +1,37 @@
+// Azraf Hossain
+// Lab 1
+// 9.10.24
+
+#include <iostream>
+
+using namespace std;
+
+int main() // this is the function titled "main"
+{
+    cout << "######################################################################################" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "# Ways to access the Task Manager on your Windows computer:                          #" << endl;
+    cout << "#    Press the key combination Ctrl + Shift + Escape                                 #" << endl;
+    cout << "#    Press the key combination Ctrl + Alt + Delete and select “Task Manager”         #" << endl;
+    cout << "#    Type “Task Manager” in the Windows Start menu search                            #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "######################################################################################" << endl;
+
+    
+
+return 0; // Reurns main
+}
+
+/*     cout << "######################################################################################" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "# Ways to access the Task Manager on your Windows computer:                          #" << endl;
+    cout << "#    Press the key combination Ctrl + Shift + Escape                                 #" << endl;
+    cout << "#    Press the key combination Ctrl + Alt + Delete and select “Task Manager”         #" << endl;
+    cout << "#    Type “Task Manager” in the Windows Start menu search                            #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "#                                                                                    #" << endl;
+    cout << "######################################################################################" << endl;
+*/
