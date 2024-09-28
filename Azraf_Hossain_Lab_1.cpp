b2200c54471def250c2f3d4fb03c6a713d4fe2b1
Azrafdabomb <azrafh@nycstudents.net>
2024-09-28 09:00:32 -0400

    AzrafHossainLab1 Redo

--- Azraf_Hossain_Lab_1.cpp
+++ Azraf_Hossain_Lab_1.cpp
@@ -2,36 +2,45 @@
 // Lab 1
 // 9.10.24
 
-#include <iostream>
+#include <iostream> // Required
 
-using namespace std;
+using namespace std; // Shortens Cout statments
 
 int main() // this is the function titled "main"
-{
-    cout << "######################################################################################" << endl;
-    cout << "#                                                                                    #" << endl;
-    cout << "#                                                                                    #" << endl;
-    cout << "# Ways to access the Task Manager on your Windows computer:                          #" << endl;
-    cout << "#    Press the key combination Ctrl + Shift + Escape                                 #" << endl;
-    cout << "#    Press the key combination Ctrl + Alt + Delete and select “Task Manager”         #" << endl;
-    cout << "#    Type “Task Manager” in the Windows Start menu search                            #" << endl;
-    cout << "#                                                                                    #" << endl;
-    cout << "#                                                                                    #" << endl;
-    cout << "######################################################################################" << endl;
+{ //Starting bracket for the code
+
+    cout << "##############################################################################" << endl; // endl; is used to create a newline, moves the cursor to the next line
+    cout << "#                                                                            #" << endl;
+    cout << "# Ways to access the Task Manager on your Windows computer:                  #" << endl;
+    cout << "#                                                                            #" << endl;    
+    cout << "#    Press the key combination Ctrl + Shift + Escape                         #" << endl;
+    cout << "#                                                                            #" << endl;
+    cout << "#    Press the key combination Ctrl + Alt + Delete and select “Task Manager” #" << endl;
+    cout << "#                                                                            #" << endl;
+    cout << "#    Type “Task Manager” in the Windows Start menu search                    #" << endl;
+    cout << "#                                                                            #" << endl;
+    cout << "##############################################################################" << endl;
 
     
 
-return 0; // Reurns main
+return 0; // all programs must end with return zero
 }
 
-/*     cout << "######################################################################################" << endl;
-    cout << "#                                                                                    #" << endl;
-    cout << "#                                                                                    #" << endl;
-    cout << "# Ways to access the Task Manager on your Windows computer:                          #" << endl;
-    cout << "#    Press the key combination Ctrl + Shift + Escape                                 #" << endl;
-    cout << "#    Press the key combination Ctrl + Alt + Delete and select “Task Manager”         #" << endl;
-    cout << "#    Type “Task Manager” in the Windows Start menu search                            #" << endl;
-    cout << "#                                                                                    #" << endl;
-    cout << "#                                                                                    #" << endl;
-    cout << "######################################################################################" << endl;
+/*     
+
+##############################################################################
+#                                                                            #
+# Ways to access the Task Manager on your Windows computer:                  #
+#                                                                            #
+#    Press the key combination Ctrl + Shift + Escape                         #
+#                                                                            #
+#    Press the key combination Ctrl + Alt + Delete and select “Task Manager” #
+#                                                                            #
+#    Type “Task Manager” in the Windows Start menu search                    #
+#                                                                            #
+##############################################################################
+
+[1] + Done                       "/usr/bin/gdb" --interpreter=mi --tty=${DbgTerm} 0<"/tmp/Microsoft-MIEngine-In-lf4beyyt.2f5" 1>"/tmp/Microsoft-MIEngine-Out-gmew2xdu.kdk"
+@Azrafdabomb ➜ /workspaces/lab-1-cout-statements-Azrafdabomb (main) $ 
+
 */
